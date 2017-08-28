28b306a8e70ae9099e3fd56155ae11eebcdfa662
Jakub Jirutka <jakub@jirutka.cz>
2017-08-28 19:28:03 +0200

    Add pragma "frozen_string_literal: true" to lib/**/*.rb

--- lib/slim/h_tag.rb
+++ lib/slim/h_tag.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'slim/h_tag/filter'
 require 'slim/h_tag/version'
 
--- lib/slim/h_tag/version.rb
+++ lib/slim/h_tag/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Slim
   module HTag
     VERSION = '0.1.0'
