e16dd3e83bd9a6c8d0557cbf6f7a0fa33df66582
Jakub Jirutka <jakub@jirutka.cz>
2017-08-28 19:49:47 +0200

    Report test coverage to Codacy

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'slim', ENV['SLIM_VERSION'] unless ENV.fetch('SLIM_VERSION', '').empty?
+
+group :ci do
+  gem 'codacy-coverage', '~> 1.1', require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,17 @@ RSpec.configure do |config|
   config.color = true
 end
 
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+if ENV['CODACY_PROJECT_TOKEN']
+  require 'codacy-coverage'
+  formatters << Codacy::Formatter
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 def slim_render(source, options = {}, &block)
   scope = options.delete(:scope)
   locals = options.delete(:locals)
